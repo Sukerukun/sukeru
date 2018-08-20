984fe31619c7f8c797ad4ae1d6b90ddbbc771b88
Sukerukun <42169854+Sukerukun@users.noreply.github.com>
2018-08-19 19:50:57 -1000

    Add files via upload

---
+++ .gitignore/errors.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+let config = require("../botconfig.json");
+
+module.exports.noPerms = (message, perm) => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setTitle("Permission insuffisante")
+        .setColor(config.red)
+        .addField("Permission nécessaire", perm);
+
+    message.channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.equalPerms = (message, user, perms) => {
+
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setColor(config.red)
+        .setTitle("Error")
+        .addField(`${user} has perms`, perms);
+
+    message.channel.send(embed).then(m => m.delete(5000));
+
+}
+
+module.exports.botuser = (message) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription("You cannot ban a bot.")
+        .setColor(config.red);
+
+    message.channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.cantfindUser = (channel) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription(":x: Erreur mauvais usage.")
+        .setColor(config.red);
+
+    channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.noReason = (channel) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription("S'il vous plaît, veiller fournir une raison.")
+        .setColor(config.red);
+
+    channel.send(embed).then(m => m.delete(5000));
+}
